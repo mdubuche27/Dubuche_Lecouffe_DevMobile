284ec96ee1d696d5f5fbf08ad6ed929eb95dbe8c
Gauthier <gauthierlcf@hotmail.fr>
2021-11-05 12:16:25 +0100

    models init

---
+++ api_rick_morty/android/app/src/main/kotlin/com/example/api_rick_morty/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.api_rick_morty
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ api_rick_morty/lib/main.dart
@@ -0,0 +1,65 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:rick_and_morty_api/rick_and_morty_api.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  Future<Map<String,dynamic>> getCharacters() async {
+    final response = await http.get(Uri.parse('https://rickandmortyapi.com/api/character'));
+    return json.decode(response.body.toString()) as Map<String,dynamic>;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            const Text('TODO'),
+            const Text('Do something')
+          ],
+        ),
+      ) // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
